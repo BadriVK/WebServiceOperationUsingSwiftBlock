dff5655a76934678fc0f3a9a7bc1cfdc34719711
arpit <arpit_vk@yahoo.co.in>
2015-10-26 15:37:47 +0530

    Initial Commit

---
+++ WebServiceOperations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WebServiceOperations
+//
+//  Created by Badri Vishwakarma on 26/10/15.
+//  Copyright © 2015 Arpit Vishwakarma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
